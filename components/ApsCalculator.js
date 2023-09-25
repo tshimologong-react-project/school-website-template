603510d2f689415ded8497283ba52c403c326b18
Lesibana Ngoepe <LCBA21>
2023-09-25 11:11:13 +0200

    feat: added feature that checks if subject has been choosen more than once

--- components/ApsCalculator.js
+++ components/ApsCalculator.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 const ApsCalculator = () => {
     const subjects = ['Mathematics', 'LifeScience', 'English','LifeOrientation','PhysicalScience','Geography','HomeLanguge'];
-    const marksOptions = ['0-29%', '30-39%', '40-40%','50-59%','60-69%','70-79%','80-100%'];
+    const marksOptions = ['0-29%', '30-39%', '40-49%','50-59%','60-69%','70-79%','80-100%'];
     const [total,setTotal] = useState(0);
   
   
@@ -53,9 +53,9 @@ const ApsCalculator = () => {
       const updatedSubjects = { ...selectedSubjects };
       setSelectedSubjects(updatedSubjects);
   
-     
-  
-     
+      updatedSubjects[subject] = Object.values(selectedSubjects).includes(selectedSubject)
+      ? (alert(`You have already selected ${selectedSubject}`), defaultSubject)
+      : selectedSubject;
     
     };
   
