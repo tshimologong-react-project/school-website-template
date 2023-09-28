699171c4325d1b4446cc092eb11fb417a655465c
mbali92 <83025186+mbali92@users.noreply.github.com>
2023-09-28 13:32:44 +0200

    Update: updated main structure on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import TopHeader from './components/TopHeader';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-    
+      <TopHeader/>
+      <Header/>
+      <Hero/>
     </div>
   );
 }
