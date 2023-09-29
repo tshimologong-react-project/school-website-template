2e67047457ffceec2535df47ce654c493e501434
mbali92 <83025186+mbali92@users.noreply.github.com>
2023-09-29 16:29:04 +0200

    Updated: updated project structure on app section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import TopHeader from './components/TopHeader';
 import Hero from './components/Hero';
 import About from './components/About';
 //import Gallery from './components/Gallery';
 import ApsCalculator from  './components/ApsCalculator'
 import GalleryPage from  './components/GalleryPage';
 
-//import Blog from './components/Blog';
-//import BlogPage from './components/BlogPage';
-//import AllBlogPage from './components/AllBlogPage';
+import Blog from './components/Blog';
+import BlogPage from './components/BlogPage';
+import AllBlogPage from './components/AllBlogPage';
+import Footer from './components/Footer';
 
 // Blog images
-{/*import img1 from './assets/IMG1.JPG';
+import img1 from './assets/IMG1.JPG';
 import img2 from './assets/IMG2.JPG';
 import img3 from './assets/IMG3.JPG';
 import img4 from './assets/IMG4.JPG';
-import img5 from './assets/IMG5.JPG';*/}
+import img5 from './assets/IMG5.JPG';
 
-{/*const blogPosts = [
+const blogPosts = [
   {
     title: 'Title one',
     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias culpa atque officiis delectus nemo quidem',
@@ -45,25 +44,26 @@ import img5 from './assets/IMG5.JPG';*/}
     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias culpa atque officiis delectus nemo quidem',
     imageUrl: img5,
   },
-];*/}
+];
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <TopHeader />
-        <Header />
-        <Hero />
-        <About />
-       <GalleryPage/>
-       < ApsCalculator/>
-        {/*<Routes>
+    <div className="App">
+      <Hero />
+      <About/>
+      <Router>
+        <Routes>
           <Route path="/" element={<Blog blogPosts={blogPosts} />} />
           <Route path="/blog-page/:title" element={<BlogPage />} />
           <Route path="/all-posts" element={<AllBlogPage blogPosts={blogPosts} />} />
-        </Routes>*/}
-      </div>
-    </Router>
+        </Routes>
+      </Router>
+      <GalleryPage/>
+      <ApsCalculator/>
+      <Footer/>
+    </div>
+    
+    
   );
 }
 
